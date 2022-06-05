92cec1d1313fa38f8ec79b3f8607dec0acbf87e9
Thomas Hartmann <thomas@webtrade.dk>
2022-06-05 14:25:13 +0200

    infinitescroll is now working

--- src/components/showimages/ShowImages.jsx
+++ src/components/showimages/ShowImages.jsx
@@ -12,7 +12,7 @@ const ShowImages = ({ tags, url, loggedIn, baseUrl, logout }) => {
     const [imagesToShow, setImagesToShow] = useState([]); // set images paginated
     const [image, setImage] = useState({ name: '', title: '', description: '', viewno: '', tags: [] });
     const [startIndex, setStartIndex] = useState(0);
-    const [hasMore, setHasMore] = useState(false);
+    // const [hasMore, setHasMore] = useState(false);
     const observer = useRef()                   // useRef is like a “box” that can hold a mutable value in its .current property. It lives as long as the components lifetime.
     const lastImageRef = useCallback(           // The useCallback Hook only runs when one of its dependencies update. Usecase: Stop the inner callback from being updated on re-render (only on changes to dependent array elements)
         (node) => {
@@ -20,7 +20,7 @@ const ShowImages = ({ tags, url, loggedIn, baseUrl, logout }) => {
             observer.current = new IntersectionObserver((entries) => { //observer is a Ref (object with single property: current). Each entry in the list of thresholds is an IntersectionObserverEntry object describing one threshold that was crossed; that is, each entry describes how much of a given element is intersecting with the root element, whether or not the element is considered to be intersecting or not, and the direction in which the transition occurred.
                 if (entries[0].isIntersecting && true) {               // If any entry is intersecting and if there are more component/images etc. to load we react.
                     setStartIndex((prev) => prev + 1);
-                    alert('HEY');
+                    // alert('HEY');
                     console.log('SCROLLING',startIndex);
                 }
             });
@@ -87,15 +87,15 @@ const ShowImages = ({ tags, url, loggedIn, baseUrl, logout }) => {
         if (startIndex === 0 && filteredImages.length > 0) {
             setImagesToShow(filteredImages.slice(0, 40));//properties.imagesPrPage));
         } else {
-            const firstImageIdx = startIndex * properties.imagesPrPage;
-            const lastImageIdx = firstImageIdx + properties.imagesPrPage;
-            // alert(`${firstImageIdx},${lastImageIdx},${filteredImages.length}`);
+            const firstImageIdx = startIndex * properties.imagesPrPage; // 40
+            let lastImageIdx = firstImageIdx + properties.imagesPrPage; // 80
             if (filteredImages.length < firstImageIdx) { return }
-            if (filteredImages.length >= firstImageIdx && lastImageIdx <= filteredImages.length) {
-                const imagesToAdd = filteredImages.slice(firstImageIdx, lastImageIdx);
-                setImagesToShow([...imagesToShow, imagesToAdd]);
-                console.log("TESTIng IMAGES", imagesToShow, imagesToAdd, filteredImages);
-
+            if (filteredImages.length >= firstImageIdx ) {
+                if(lastImageIdx > filteredImages.length - 1) { // We are on the last batch
+                    lastImageIdx = filteredImages.length;
+                }
+                const imagesToAdd = filteredImages.slice(firstImageIdx, lastImageIdx - 1);
+                setImagesToShow([...imagesToShow, ...imagesToAdd]);
             }
         }
     }, [startIndex, filteredImages])
@@ -128,7 +128,8 @@ const ShowImages = ({ tags, url, loggedIn, baseUrl, logout }) => {
                     return (
                         // <span ref={lastImageRef}>
                             <SingleImage
-                                ref={lastImageRef}
+                                innerRef={lastImageRef}
+                                // key={image.name}
                                 key={image.name}
                                 name={image.name}
                                 viewno={image.viewno}
@@ -140,6 +141,7 @@ const ShowImages = ({ tags, url, loggedIn, baseUrl, logout }) => {
                 }
                 return (
                     <SingleImage
+                        // key={image.name}
                         key={image.name}
                         name={image.name}
                         viewno={image.viewno}
@@ -169,8 +171,8 @@ const SingleImage = (props) => {
         <span onClick={props.handleClick} name={props.name} ref={props.ref}
         // style={{ position: 'relative', textAlign: 'center' }}
         >
-            <img src={props.src} alt="" />
-            {/* {props.loggedIn &&  */}
+            {props.innerRef?<img src={props.src} ref={props.innerRef} alt="" />:<img src={props.src} alt="" />}
+                        {/* {props.loggedIn &&  */}
             {/* // <span style={{ position: 'absolute', bottom: '4px', right: '16px', color: 'red' }}>
             //     {props.viewno}
             // </span> */}
